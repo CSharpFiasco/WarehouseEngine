ecb9770b6f103489677675f8f78fd72666671a83
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-12-29 17:10:35 -0600

    move customer api to minimal api (#40)
    * move customer api to minimal api
    
    * update packages

---
+++ src/WarehouseEngine.Api/Endpoints/CustomerEndpoints.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using WarehouseEngine.Api.Extensions.ErrorTypeExtensions;
+using WarehouseEngine.Application.Dtos;
+using WarehouseEngine.Application.Interfaces;
+
+namespace WarehouseEngine.Api.Endpoints;
+
+internal class CustomerEndpoints
+{
+    public static void Map(WebApplication app)
+    {
+        app.MapGet("/api/v{version:apiVersion}/customer/{id}", [Authorize] async (ILogger<CustomerEndpoints> logger, ICustomerService customerService, Guid id) =>
+        {
+            var customer = await customerService.GetByIdAsync(id);
+            return customer.Match<Results<Ok<CustomerResponseDto>, ProblemHttpResult>>(
+                    customer => TypedResults.Ok(customer),
+                    invalidResult =>
+                    {
+                        logger.LogError("Record not found. {message}", invalidResult.GetMessage());
+                        return TypedResults.Problem(statusCode: 404, detail: invalidResult.GetMessage());
+                    });
+        })
+        .Produces<CustomerResponseDto>(200)
+        .WithName("GetCustomerById")
+        .WithTags("Customer");
+
+        app.MapGet("/api/v{version:apiVersion}/customer/count", [Authorize] async (ICustomerService customerService) =>
+        {
+            var count = await customerService.GetCount();
+            return TypedResults.Ok(count);
+        })
+        .Produces<int>(200)
+        .WithName("GetCustomerCount")
+        .WithTags("Customer");
+
+        app.MapPost("/api/v{version:apiVersion}/customer", [Authorize] async (ILogger < CustomerEndpoints > logger, ICustomerService customerService, PostCustomerDto customerDto, HttpContext httpContext) =>
+        {
+            customerDto.DateCreated = DateTime.UtcNow;
+            customerDto.CreatedBy = httpContext.User.Identity?.Name ?? "Unknown";
+
+            var customer = await customerService.AddAsync(customerDto, httpContext.User.Identity?.Name ?? "Unknown");
+
+            return customer.Match<Results<Created, ProblemHttpResult>>(
+               customer => TypedResults.Created(),
+               entityExists =>
+               {
+                   logger.LogWarning("Record not found. {message}", entityExists.GetMessage());
+
+                   return TypedResults.Problem(statusCode: 404, extensions: [new("Record not found", entityExists.GetMessage())]);
+               },
+               invalidShippingResult =>
+               {
+                   logger.LogWarning(invalidShippingResult.ErrorMessage!);
+
+                   return TypedResults.Problem(invalidShippingResult.ErrorMessage, statusCode: 404);
+               });
+        })
+        .Produces<CustomerResponseDto>(200)
+        .ProducesProblem(400)
+        .WithName("CreateCustomer")
+        .WithTags("Customer");
+    }
+}
--- tests/WarehouseEngine.Api.Integration.Tests/CustomerEndpointTests.cs
+++ tests/WarehouseEngine.Api.Integration.Tests/CustomerEndpointTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
 using Microsoft.Extensions.Options;
 using WarehouseEngine.Api.Controllers;
@@ -7,6 +8,7 @@ using WarehouseEngine.Api.Integration.Tests.Factories;
 using WarehouseEngine.Application.Dtos;
 using WarehouseEngine.Application.Implementations;
 using WarehouseEngine.Domain.Models.Auth;
+using WarehouseEngine.Domain.ValueObjects;
 using Xunit;
 
 namespace WarehouseEngine.Api.Integration.Tests;
@@ -21,28 +23,26 @@ public class CustomerEndpointTests
     }
 
     [Fact(DisplayName = $"""
-        Given a request to the {nameof(CustomerController)}
+        Given the client sends a request to retrieve a customer
         When the request is unauthorized
         Then the response should have a 401 status code
         """)]
-    public async Task CustomerController_NoAuth_Unauthorized()
+    public async Task CustomerController_GET_NoAuth_Unauthorized()
     {
-        // Arrange
         using var client = _factory.CreateClient();
-        // Act
-        using var response = await client.GetAsync("api/v1/Customer", TestContext.Current.CancellationToken);
-        // Assert
-        // response should have 401 status code
+
+        using var response = await client.GetAsync($"api/v1/Customer/{WarehouseEngineFactory.CustomerId1}", TestContext.Current.CancellationToken);
+
         Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
     }
 
     [Fact(DisplayName = $"""
-        Given a request to the {nameof(CustomerController)}
+        Given the client sends a request to retrieve a customer
         When the request is authorized
         And when the requested resource is not found
         Then the response should have a 404 status code
         """)]
-    public async Task CustomerController_Auth_NotFound()
+    public async Task CustomerController_GET_Auth_NotFound()
     {
         var options = Options.Create(new JwtConfiguration
         {
@@ -57,25 +57,23 @@ public class CustomerEndpointTests
 
         var authHeader = new AuthenticationHeaderValue("Bearer", tokenString);
 
-        // Arrange
         using var client = _factory.CreateClient();
         client.DefaultRequestHeaders.Authorization = authHeader;
 
-        // Act
+
         var guid = Guid.NewGuid();
-        using var response = await client.GetAsync($"api/v1/Customer?id={guid}", TestContext.Current.CancellationToken);
-        // Assert
-        // response should have 200 status code
+        using var response = await client.GetAsync($"api/v1/Customer/{guid}", TestContext.Current.CancellationToken);
+
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 
     [Fact(DisplayName = $"""
-        Given a request to the {nameof(CustomerController)}
+        Given the client sends a request to retrieve a customer
         When the request is authorized
         And when the requested resource is found
         Then the response should have a 200 status code
         """)]
-    public async Task CustomerController_Auth_Found()
+    public async Task CustomerController_GET_Auth_Found()
     {
         var options = Options.Create(new JwtConfiguration
         {
@@ -90,14 +88,11 @@ public class CustomerEndpointTests
 
         var authHeader = new AuthenticationHeaderValue("Bearer", tokenString);
 
-        // Arrange
         using var client = _factory.CreateClient();
         client.DefaultRequestHeaders.Authorization = authHeader;
 
-        // Act
-        using var response = await client.GetAsync($"api/v1/Customer?id={WarehouseEngineFactory.CustomerId1}", TestContext.Current.CancellationToken);
-        // Assert
-        // response should have 200 status code
+        using var response = await client.GetAsync($"api/v1/Customer/{WarehouseEngineFactory.CustomerId1}", TestContext.Current.CancellationToken);
+
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         var contentString = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
         var deserializedCustomerDto = JsonSerializer.Deserialize<CustomerResponseDto>(contentString, JsonSerializerOptions.Web);
@@ -107,6 +102,159 @@ public class CustomerEndpointTests
 
         Assert.NotNull(deserializedCustomerDto.ShippingAddress);
         Assert.Equal("OK", deserializedCustomerDto.ShippingAddress.State);
+    }
 
+    [Fact(DisplayName = $"""
+        Given the client sends a request to create a customer
+        When the request is unauthorized
+        Then the response should have a 401 status code
+        """)]
+    public async Task CustomerController_POST_NoAuth_Unauthorized()
+    {
+        using var client = _factory.CreateClient();
+
+        var customerPayload = new PostCustomerDto
+        {
+            Name = "New Customer",
+            ShippingAddress = new Address
+            {
+                Address1 = "123 Main St",
+                City = "Anytown",
+                State = "CA",
+                ZipCode = "12345",
+            }
+        };
+
+        using var customerContent = JsonContent.Create(customerPayload);
+
+        using var response = await client.PostAsync($"api/v1/Customer", customerContent, TestContext.Current.CancellationToken);
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact(DisplayName = $"""
+        Given the client sends a request to create a customer
+        When the request is authorized
+        Then the response should have a 201 status code
+        """)]
+    public async Task CustomerController_POST_Auth_NotFound()
+    {
+        var options = Options.Create(new JwtConfiguration
+        {
+            Secret = "MyIntegrationTestSecr3!tIsSoSecr3t",
+            ValidIssuer = "http://localhost",
+            ValidAudience = "http://warehouse-api"
+        });
+        var jwtService = new JwtService(options);
+
+
+        var tokenString = jwtService.GetNewToken([]);
+
+        var authHeader = new AuthenticationHeaderValue("Bearer", tokenString);
+
+        using var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = authHeader;
+
+        var customerPayload = new PostCustomerDto
+        {
+            Name = "New Customer",
+            ShippingAddress = new Address
+            {
+                Address1 = "123 Main St",
+                City = "Anytown",
+                State = "CA",
+                ZipCode = "12345",
+            }
+        };
+
+        using var customerContent = JsonContent.Create(customerPayload);
+        using var response = await client.PostAsync($"api/v1/Customer", customerContent, TestContext.Current.CancellationToken);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    [Fact(DisplayName = $"""
+        Given the client sends a request to create a customer
+        When the request is authorized
+        And the request has an id
+        Then the response should have a 201 status code
+        And the customer should be created with a different id
+        """)]
+    public async Task CustomerController_POST_Auth_WithId ()
+    {
+        var options = Options.Create(new JwtConfiguration
+        {
+            Secret = "MyIntegrationTestSecr3!tIsSoSecr3t",
+            ValidIssuer = "http://localhost",
+            ValidAudience = "http://warehouse-api"
+        });
+        var jwtService = new JwtService(options);
+
+
+        var tokenString = jwtService.GetNewToken([]);
+
+        var authHeader = new AuthenticationHeaderValue("Bearer", tokenString);
+
+        using var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = authHeader;
+
+        var id = Guid.NewGuid();
+
+        var customerPayload = new PostCustomerDto
+        {
+            Id = id,
+            Name = "New Customer",
+            ShippingAddress = new Address
+            {
+                Address1 = "123 Main St",
+                City = "Anytown",
+                State = "CA",
+                ZipCode = "12345",
+            }
+        };
+
+        using var customerContent = JsonContent.Create(customerPayload);
+        using var response = await client.PostAsync($"api/v1/Customer", customerContent, TestContext.Current.CancellationToken);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        using var getResponse = await client.GetAsync($"api/v1/Customer/{id}", TestContext.Current.CancellationToken);
+        Assert.Equal(HttpStatusCode.NotFound, getResponse.StatusCode);
+    }
+
+    [Fact(DisplayName = $"""
+        Given the client sends a request to create a customer
+        When the request is authorized
+        And when the customer has no shipping address
+        Then the response should have a 400 status code
+        """)]
+    public async Task CustomerController_POST_NoShipping()
+    {
+        var options = Options.Create(new JwtConfiguration
+        {
+            Secret = "MyIntegrationTestSecr3!tIsSoSecr3t",
+            ValidIssuer = "http://localhost",
+            ValidAudience = "http://warehouse-api"
+        });
+        var jwtService = new JwtService(options);
+
+
+        var tokenString = jwtService.GetNewToken([]);
+
+        var authHeader = new AuthenticationHeaderValue("Bearer", tokenString);
+
+        using var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = authHeader;
+
+        var customerPayload = new PostCustomerDto
+        {
+            Name = "New Customer",
+            ShippingAddress = null!
+        };
+
+        using var customerContent = JsonContent.Create(customerPayload);
+        using var response = await client.PostAsync($"api/v1/Customer", customerContent, TestContext.Current.CancellationToken);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }

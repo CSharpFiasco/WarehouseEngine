e80c0fed5931dabc0760b32e1533f9309ef38c0c
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2026-01-24 17:55:45 -0600

    updates and adjustments

--- src/WarehouseEngine.UI/src/app/app.component.ts
+++ src/WarehouseEngine.UI/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { LoginComponent } from './pages/login/login.component';
 import { RouterOutlet } from '@angular/router';
 import { AuthFacade } from './store/auth/auth.facade';
 
@@ -12,7 +11,7 @@ import { AuthFacade } from './store/auth/auth.facade';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, TopNavComponent, SidenavComponent, LoginComponent, MatProgressSpinnerModule, RouterOutlet]
+    imports: [CommonModule, TopNavComponent, SidenavComponent, MatProgressSpinnerModule, RouterOutlet]
 })
 export class AppComponent {
   private readonly authFacade: AuthFacade = inject(AuthFacade);
